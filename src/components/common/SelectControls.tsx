2a1cd032c96ce1e82658fb2481459f6a0a854623
Elena Sedelnikova <lena1687@gmail.com>
2025-10-09 12:52:05 +0200

    Products list: filter 'Category' - SelectControls

---
+++ src/components/common/SelectControls.tsx
@@ -0,0 +1,58 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+type SelectControlsProps = {
+  label: string;
+  options: Record<string, string>[];
+  value: string;
+  onChangeValue: (value: string) => void;
+  isLoading: boolean;
+  isError: boolean;
+  error: unknown;
+};
+
+const SelectControls = ({
+  label,
+  options,
+  value,
+  onChangeValue,
+  isLoading,
+  isError,
+  error,
+}: SelectControlsProps) => {
+  const textValue = isLoading || isError ? '' : value || '';
+
+  return (
+    <FormControl sx={{ my: 3, minWidth: 200 }}>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        value={textValue}
+        onChange={(e) => onChangeValue(e.target.value)}
+        disabled={isLoading || isError}
+        autoWidth
+        label={label}
+      >
+        {isError && (
+          <MenuItem value="" disabled>
+            Error loading options: {(error as Error).message}
+          </MenuItem>
+        )}
+        {isLoading && (
+          <MenuItem value="" disabled>
+            Loading options...
+          </MenuItem>
+        )}
+        {!isLoading &&
+          !isError && [
+            <MenuItem value="">All options</MenuItem>,
+            ...options.map(({ id, name }) => (
+              <MenuItem key={id} value={id}>
+                {name}
+              </MenuItem>
+            )),
+          ]}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SelectControls;
--- src/components/productCard/ProductFilters.tsx
+++ src/components/productCard/ProductFilters.tsx
@@ -1,7 +1,7 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import type { CategoryApiResponseType } from '../../types/products.ts';
 import type { UseQueryResult } from '@tanstack/react-query';
+import SelectControls from '../common/SelectControls.tsx';
 
 type ProductFiltersProps = {
   initialParams: Record<string, string>;
@@ -20,8 +20,10 @@ const ProductFilters = ({ initialParams, categoriesData, category }: ProductFilt
     error: categoriesError,
   } = categoriesData;
 
-  const categoryOptions = Array.isArray(categories) ? categories : [];
-  const validCategory = categoryOptions.some((cat) => cat.slug === category) ? category : '';
+  const categoryOptions = Array.isArray(categories)
+    ? categories.map(({ slug, name }) => ({ id: slug, name: name }))
+    : [];
+  const validCategory = categoryOptions.some((cat) => cat.id === category) ? category || '' : '';
 
   const handleCategory = (selectedCategory: string) => {
     if (selectedCategory) {
@@ -38,36 +40,15 @@ const ProductFilters = ({ initialParams, categoriesData, category }: ProductFilt
   };
 
   return (
-    <FormControl sx={{ my: 3, minWidth: 200 }}>
-      <InputLabel>Category</InputLabel>
-      <Select
-        value={isLoadingCategories || isCategoriesError ? '' : validCategory || ''}
-        onChange={(e) => handleCategory(e.target.value)}
-        disabled={isLoadingCategories || isCategoriesError}
-        autoWidth
-        label="Category"
-      >
-        {isCategoriesError && (
-          <MenuItem value="" disabled>
-            Error loading categories: {(categoriesError as Error).message}
-          </MenuItem>
-        )}
-        {isLoadingCategories && (
-          <MenuItem value="" disabled>
-            Loading categories...
-          </MenuItem>
-        )}
-        {!isLoadingCategories &&
-          !isCategoriesError && [
-            <MenuItem value="">All Categories</MenuItem>,
-            ...categoryOptions.map((cat) => (
-              <MenuItem key={cat.slug} value={cat.slug}>
-                {cat.name}
-              </MenuItem>
-            )),
-          ]}
-      </Select>
-    </FormControl>
+    <SelectControls
+      label="Category"
+      options={categoryOptions}
+      value={validCategory}
+      onChangeValue={handleCategory}
+      isLoading={isLoadingCategories}
+      isError={isCategoriesError}
+      error={categoriesError}
+    />
   );
 };
 
